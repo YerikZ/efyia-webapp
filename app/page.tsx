618a3e08b6272ed855414e6a59c1b35cdb4e9ef9
YerikZ <yerik.yilinzhu@gmail.com>
2024-07-15 18:12:31 +0100

    add session expire countdown

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,65 @@
 "use client";
-import React from 'react'
 
 import Main from '@/app/components'
-import { useState } from 'react';
+import React, {useState, useEffect } from 'react';
 import Overlay from '@/app/components/overlay-page'
 
 const App = () => {
 
   const [isAccessGranted, setIsAccessGranted] = useState(false);
 
+  const [countdown, setCountdown] = useState(15 * 60); // 15 minutes in seconds
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+    let interval: NodeJS.Timeout;
+    if (isAccessGranted) {
+      // Set a timer to refresh the page after 15 minutes
+      timer = setTimeout(() => {
+        window.location.reload();
+      }, 15 * 60 * 1000); // 15 minutes in milliseconds
+
+      // Set an interval to update the countdown every second
+      interval = setInterval(() => {
+        setCountdown(prevCountdown => {
+          if (prevCountdown <= 1) {
+            clearInterval(interval);
+            return 0;
+          }
+          return prevCountdown - 1;
+        });
+      }, 1000);
+    }
+
+    return () => {
+      // Clear the timer and interval if the component unmounts or if the access is no longer granted
+      if (timer) clearTimeout(timer);
+      if (interval) clearInterval(interval);
+    };
+  }, [isAccessGranted]);
+
+  const formatTime = (timeInSeconds: number) => {
+    const minutes = Math.floor(timeInSeconds / 60);
+    const seconds = timeInSeconds % 60;
+    return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+  };
+
+  if (!isAccessGranted) {
+    return <Overlay onAccessGranted={() => setIsAccessGranted(true)} />;
+  }
+
   if (!isAccessGranted) {
     return <Overlay onAccessGranted={() => setIsAccessGranted(true)} />;
   }
 
   return (
-    <Main />
-  )
+    <div>      
+      <div className="countdown-timer">
+        Session expires in: {formatTime(countdown)}        
+      </div>
+      <Main />
+    </div>
+  );
 }
 
 export default React.memo(App)
