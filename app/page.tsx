bcd907eff6f822c8ee2d6ed512f6be0af545caf1
YerikZ <yerik.yilinzhu@gmail.com>
2024-07-15 17:57:21 +0100

    add overlay page to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
+"use client";
 import React from 'react'
 
 import Main from '@/app/components'
+import { useState } from 'react';
+import Overlay from '@/app/components/overlay-page'
 
 const App = () => {
+
+  const [isAccessGranted, setIsAccessGranted] = useState(false);
+
+  if (!isAccessGranted) {
+    return <Overlay onAccessGranted={() => setIsAccessGranted(true)} />;
+  }
+
   return (
     <Main />
   )
